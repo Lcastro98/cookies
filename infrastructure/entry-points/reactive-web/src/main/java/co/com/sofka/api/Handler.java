ee47b5b543cd1fac826970b23018dc89ab73125e
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-03 17:00:00 -0500

    Se crea el servicio para poder consultar un tipo de galleta por código

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/Handler.java
@@ -4,25 +4,32 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
 public class Handler {
-//private  final UseCase useCase;
-//private  final UseCase2 useCase2;
-    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
-        // usecase.logic();
-        return ServerResponse.ok().bodyValue("");
+    public Mono<ServerResponse> listenGETUseCaseGetCookie(ServerRequest serverRequest) {
+        String cookie = serverRequest.queryParam("id").orElse("Id no encontrado");
+        return ServerResponse.ok().body(getAnCookie(cookie), Flux.class);
     }
 
-    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
-        // useCase2.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
+    public Flux<Cookie> getAnCookie(String id) {
 
-    public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
-        // usecase.logic();
-        return ServerResponse.ok().bodyValue("");
+        Flux<Cookie> cookieTypes = Flux.fromIterable(Arrays.asList(
+                new Cookie("1", "Mantequilla"), new Cookie("2", "Chocolate"),
+                new Cookie("3", "Vainilla"), new Cookie("4", "Avena"),
+                new Cookie("5", "Coco"), new Cookie("6", "Jengibre"),
+                new Cookie("7", "Chispas"), new Cookie("8", "Macadamia"),
+                new Cookie("9", "Avellana"), new Cookie("10", "Especial")
+        ));
+
+        return cookieTypes
+                .filter(type -> type.getId().equals(id))
+                .switchIfEmpty(Mono.error(new Exception("No existe una galleta con este nombre")))
+                .onErrorResume(error -> Mono.just(new Cookie(id, "Tipo de galleta no encontrado")));
     }
 }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/RouterRest.java
@@ -13,8 +13,7 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 public class RouterRest {
 @Bean
 public RouterFunction<ServerResponse> routerFunction(Handler handler) {
-    return route(GET("/api/usecase/path"), handler::listenGETUseCase)
-    .andRoute(POST("/api/usecase/otherpath"), handler::listenPOSTUseCase).and(route(GET("/api/otherusercase/path"), handler::listenGETOtherUseCase));
+    return route(GET("/api/usecase/cookieType"), handler::listenGETUseCaseGetCookie);
 
     }
 }
